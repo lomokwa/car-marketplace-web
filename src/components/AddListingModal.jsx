22e10f5ea7695a6a4a8d802e8d65d6f5082fedc7
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-08-07 16:45:55 -0400

    removed requirement for listing url

--- src/components/AddListingModal.jsx
+++ src/components/AddListingModal.jsx
@@ -254,7 +254,7 @@ export default function AddListingModal({ setShowListingModal, setCarGrid, makeL
                   <input
                     onChange={(e) => setUrl(e.target.value)}
                     value={url}
-                    placeholder="Listing URL"
+                    placeholder="Listing URL (optional)"
                     name="url"
                     type="string"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-100 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
