9d3c0d66296c8c4255c9efa35413cb2d57559cdf
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-08-07 15:03:29 -0400

    fixed light mode

--- src/app/cars/CarCard.jsx
+++ src/app/cars/CarCard.jsx
@@ -13,14 +13,14 @@ export default function CarCard({ car }) {
   return(
     <>
       {showListingPageModal && <ListingPageModal car={car} setShowListingPageModal={setShowListingPageModal} />} 
-        <div onClick={handleClick} className="m-3 my-5 w-full md:w-[18%] cursor-pointer bg-gray-900 p-5 rounded-md">
+        <div onClick={handleClick} className="m-3 my-5 w-full md:w-[18%] cursor-pointer bg-gray-100 dark:bg-gray-900 p-5 rounded-md">
           <div className="block relative h-48 rounded overflow-hidden">
             <img alt={`${car.make} ${car.model}`} className="object-cover object-center w-full h-full block" src={car.image} />
           </div>
           <div className="mt-4">
-            <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{car.year}</h3>
-            <h2 className="text-white title-font text-lg font-medium">{car.make} {car.model}</h2>
-            <p className="mt-1">${car.price.toLocaleString()} | {car.mileage.toLocaleString()} Miles</p>
+            <h3 className="text-gray-700 dark:text-gray-500 text-xs tracking-widest title-font mb-1">{car.year}</h3>
+            <h2 className="text-gray-900 dark:text-white title-font text-lg font-medium">{car.make} {car.model}</h2>
+            <p className="mt-1 text-gray-700 dark:text-gray-100 ">${car.price.toLocaleString()} | {car.mileage.toLocaleString()} Miles</p>
           </div>
         </div>
     </>
--- src/components/ListingPageModal.jsx
+++ src/components/ListingPageModal.jsx
@@ -45,9 +45,9 @@ export default function ListingPageModal ({ car, setShowListingPageModal }) {
   return(
     <>
       <div className="justify-center items-center flex overflow-x-hidden fixed inset-0 z-50 outline-none focus:outline-none h-full w-full ">
-        <section className="text-gray-200 bg-gray-900 body-font overflow-hidden">
+        <section className="bg-gray-100 dark:text-gray-200 dark:bg-gray-900 body-font overflow-hidden">
           <button
-            className="top-0 left-0 ml-4 mt-4 text-gray-300 text-2xl bg-red-500 rounded-lg px-3 pb-1 hover:bg-red-600 focus:outline-none"
+            className="top-0 left-0 ml-4 mt-4 text-gray-800 dark:text-gray-300 text-2xl bg-red-500 rounded-lg px-3 pb-1 hover:bg-red-600 focus:outline-none"
             onClick={() => setShowListingPageModal(false)}>
             &times;
           </button>
@@ -57,8 +57,8 @@ export default function ListingPageModal ({ car, setShowListingPageModal }) {
                 <img alt={`${car.make} ${car.model}`} src={car.image} className="md:h-[400px] w-full object-cover" />
               </a>
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 className="text-sm title-font text-gray-300 tracking-widest">{car.year}</h2>
-                <h1 className="text-gray-100 text-3xl title-font font-medium mb-1">{car.make} {car.model}</h1>
+                <h2 className="text-sm title-font text-gray-700 dark:text-gray-300 tracking-widest">{car.year}</h2>
+                <h1 className="text-gray-900 dark:text-gray-100 text-3xl title-font font-medium mb-1">{car.make} {car.model}</h1>
                 <div className="flex mb-4">
                   <span className="flex items-center">
                     <svg stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="fill-red-500 w-5 h-5" viewBox="0 0 24 24">
@@ -72,17 +72,17 @@ export default function ListingPageModal ({ car, setShowListingPageModal }) {
                     </a>
                   </span>
                 </div>
-                <p className="leading-relaxed w-fit md:w-auto h-24 md:h-36 overflow-y-auto">{!car.description ? "No description available." : `"${car.description}"`}</p>
+                <p className="leading-relaxed w-fit md:w-auto h-24 md:h-36 overflow-y-auto text-gray-900 dark:text-gray-100">{!car.description ? "No description available." : `"${car.description}"`}</p>
                 <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                   <div className="flex">
-                    <span className="mr-3 font-bold">{car.transmission} transmission</span>
+                    <span className="mr-3 font-bold text-gray-600 dark:text-gray-100">{car.transmission} transmission</span>
                   </div>
                   <div className="flex ml-6 items-center font-bold">
-                    <span className="mr-3">{car.mileage.toLocaleString()} Miles</span>
+                    <span className="mr-3 text-gray-600 dark:text-gray-100">{car.mileage.toLocaleString()} Miles</span>
                   </div>
                 </div>
                 <div className="flex">
-                  <span className="title-font font-medium text-2xl text-gray-100">${car.price.toLocaleString()}</span>
+                  <span className="title-font font-medium text-2xl text-gray-900 dark:text-gray-100">${car.price.toLocaleString()}</span>
                   <button onClick={handleRatingClick} className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                     <svg stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className={`w-5 h-5 ${hasClickedButton && "fill-red-500"}`} viewBox="0 0 24 24">
                       <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
